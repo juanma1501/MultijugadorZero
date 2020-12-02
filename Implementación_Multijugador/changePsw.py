02b72935611084cc95e8e3f46d75c8688556f7d9
ernestoag20 <ernesto.plata@alu.uclm.es>
2020-12-03 00:29:45 +0100

    Implementación del cliente cambiar contraseña

---
+++ Implementación_Multijugador/changePsw.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import Ice
+import hashlib
+import sys
+from getpass import getpass
+Ice.loadSlice('icegauntlet.ice')
+import IceGauntlet
+
+#Recibiremos por parámetro un proxy, y pediremos usuario y contraseña por teclado
+
+if len(sys.argv) < 2:
+    print("Introduce el proxy por parámetro.")
+    sys.exit(0)  
+
+with Ice.initialize(sys.argv) as communicator:
+    proxy_auth = IceGauntlet.AuthenticationPrx.checkedCast(communicator.stringToProxy(sys.argv[1]))
+
+    print("Introduce tu nombre de usuario:")
+    usuario = input()  #Existe el usuario?
+
+    print("Introduce la antigua contraseña:")
+    psw_antigua = getpass()
+    psw_antigua = hashlib.sha224(psw_antigua.encode()).hexdigest()
+
+    print("Introduce la contraseña nueva")
+    psw_nueva = getpass()
+    psw_nueva = hashlib.sha224(psw_nueva.encode()).hexdigest()
+
+    proxy_auth.changePassword(str(usuario), psw_antigua, psw_nueva)
+    print("La contraseña se ha cambiado con éxito.")
