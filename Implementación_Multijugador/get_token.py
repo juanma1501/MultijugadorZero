d186cb50b37bf7233f51c19d1d5f334778fd5674
JoseCarlosGualo <josecarlos.gualo@alu.uclm.es>
2020-12-04 01:11:35 +0100

    Implemetacion del cliente get_token.py

---
+++ Implementación_Multijugador/get_token.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import Ice
+import sys
+import hashlib
+from getpass import getpass
+Ice.loadSlice('icegauntlet.ice')
+import IceGauntlet
+
+if len(sys.argv < 2):
+    print("Hay que introducir el proxy")
+
+
+with Ice.initialize(sys.argv) as communicator:
+
+    print("Introduce tu nombre de usuario: ")
+    user = input()
+
+    password = getpass()
+    password = hashlib.sha224(password.encode()).hexdigest()
+
+    auth = IceGauntlet.authenticationPrx.checkedCast(communicator.stringToProxy(sys.argv[1]))
+
+    tkn = auth.getNewToken(user, password)
+    print("El token es : " + str(tkn))
