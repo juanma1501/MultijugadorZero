ad21b2e3fa2bcd1f1e01d897c93290ed6a8bea12
JoseCarlosGualo <josecarlos.gualo@alu.uclm.es>
2020-12-05 01:00:21 +0100

    Primera version del servidor de mapas

---
+++ Implementación_Multijugador/maps_server.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+import logging
+import json
+import Ice
+Ice.loadSlice('icegauntlet.ice')
+import IceGauntlet
+
+
+
+class RoomManagerI(IceGauntlet.RoomManager):
+    def __init__(self, auth_server):
+        self.maps = []
+        self.auth_server = auth_server
+
+    def publish(self, tkn, room_json):
+        if self.auth_server.isValid(tkn):
+            if json.loads(room_json) in self.maps:
+                raise IceGauntlet.RoomAlreadyExists()
+            else:
+                self.maps.append(json.loads(room_json))
+        else:
+            print("ERROR. El token introducido no es correcto.")
+
+    def remove(self, tkn, room_name):
+        if self.auth_server.isValid(tkn):
+
+            if self.exist(room_name):
+                for map in self.maps:
+                    if map['room'] and map['room'] == room_name:
+                        self.maps.remove(map)
+            else:
+                raise IceGauntlet.RoomNotExists()
+        else:
+            print("ERROR. El token introducido no es correcto.")
+
+    def getRoom(self):
+        return json.dumps(self.maps)
+
+    def exist(self, room_name):
+        encontrado = False
+        for map in self.maps:
+            if map['room'] == room_name:
+                encontrado = True
+                return encontrado
+        return encontrado
+
+    def shutdown(self, current):
+        current.adapter.getCommunicator().shutdown()
+
+    class Server(Ice.Application):
+        def run(self, args):
+            with Ice.initialize(sys.argv) as communicator:
+                logging.debug('Initializing server...')
+                prx_auth = IceGauntlet.AuthenticationPrx.checkedCast(communicator.stringToProxy("authentication -t -e 1.1:tcp -h localhost -p 10000 -t 60000"))
+                servant = RoomManagerI(prx_auth)
+                adapter = communicator.createObjectAdapterWithEndpoints("MapsAdapter", "default -h localhost -p 10001")
+                proxy = adapter.add(servant, self.communicator().stringToIdentity('maps'))
+                adapter.addDefaultServant(servant, '')
+                adapter.activate()
+                logging.debug('Adapter ready, servant proxy: {}'.format(proxy))
+                print('"{}"'.format(proxy), flush=True)
+
+                logging.debug('Entering server loop...')
+                self.shutdownOnInterrupt()
+                self.communicator().waitForShutdown()
+
+                return 0
+    if __name__ == '__main__':
+        app = Server()
+        sys.exit(app.main(sys.argv))
